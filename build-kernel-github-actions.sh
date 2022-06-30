4aee76f6b4a68c180200bf0e06574d5826188f09
MargaretaWhite23 <108430598+MargaretaWhite23@users.noreply.github.com>
2022-06-30 04:41:55 +0100

    Update build-kernel-github-actions.sh

--- build-kernel-github-actions.sh
+++ build-kernel-github-actions.sh
@@ -4,7 +4,7 @@ set -e o pipefail
 #https://wiki.debian.org/HowToUpgradeKernel
 #5.10.0-15-amd64
 #linux-image-5.10.0-15-amd64
-#echo "deb http://deb.debian.org/debian unstable main" >> /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian unstable main" > /etc/apt/sources.list
 apt update
 apt install -y linux-image-5.10.0-15-amd64 linux-source fakeroot
 apt install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev bc
