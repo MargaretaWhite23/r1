ef43d72fab093653779aa9aab87780d69cc091ce
MargaretaWhite23 <108430598+MargaretaWhite23@users.noreply.github.com>
2022-07-02 00:04:49 +0100

    Update testdocker.sh

--- testdocker.sh
+++ testdocker.sh
@@ -4,8 +4,14 @@
 docker pull debian:unstable
 
 #docker image list
-docker run -dit b7ea56f7177f
+
+#Map TCP port 22 in the container to port 2280 on the Docker host.
+docker run -dit b7ea56f7177f -p 2280:22
 docker attach b6f07fce1f62
+
+##add port mapping
+#docker port b6f07fce1f62 80
+#https://www.cloudbees.com/blog/docker-expose-port-what-it-means-and-what-it-doesnt-mean
 
 ##OLD UNUSUED
 #docker container create b7ea56f7177f
