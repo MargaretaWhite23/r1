a972b3d675d9b01496fb6244879cb76735ed5bfc
MargaretaWhite23 <108430598+MargaretaWhite23@users.noreply.github.com>
2022-07-01 23:32:28 +0100

    Update build-kernel-github-actions.sh

--- build-kernel-github-actions.sh
+++ build-kernel-github-actions.sh
@@ -6,7 +6,7 @@ build_qemu () {
   #####BUILD QEMU
   #https://www.qemu.org/download/
   #https://wiki.qemu.org/Testing/DockerBuild
-  sudo apt-get install git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build -y
+  sudo apt-get install git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build python3 -y
   mkdir ~/qemu; cd ~/qemu
   curl -L https://download.qemu.org/qemu-7.0.0.tar.xz -o qemu.tar.xz
   tar xvJf qemu.tar.xz
