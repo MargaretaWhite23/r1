448e020c6fbb1d0edddd3e9557895d47bf70c17f
MargaretaWhite23 <108430598+MargaretaWhite23@users.noreply.github.com>
2022-07-02 01:02:39 +0100

    Update testdocker.sh

--- testdocker.sh
+++ testdocker.sh
@@ -9,6 +9,10 @@ docker pull debian:unstable
 docker run -dit b7ea56f7177f -p 2280:22
 docker attach b6f07fce1f62
 
+#install software
+apt install -y screen vim ssh unzip curl
+cp ~/config-5.18.0-2-amd64 /
+
 ##add port mapping
 #docker port b6f07fce1f62 80
 #https://www.cloudbees.com/blog/docker-expose-port-what-it-means-and-what-it-doesnt-mean
